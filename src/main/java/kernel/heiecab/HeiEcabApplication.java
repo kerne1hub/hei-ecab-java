5dae96651d7df570f0c7eadc38bbeffc2812b5ee
Kernel <kerstal104@gmail.com>
2021-09-26 11:43:33 +0600

    Initial commit

---
+++ src/main/java/kernel/heiecab/HeiEcabApplication.java
@@ -0,0 +1,11 @@
+package kernel.heiecab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiEcabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HeiEcabApplication.class, args);
+    }
+}
